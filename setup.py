6c865687795598ef3240a52509a8841f24e5a96d
Sean Martin <seankieran.m@hotmail.com>
2020-07-28 14:23:24 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ DISTNAME = "skm_pyutils"
 MAINTAINER = "Sean Martin"
 MAINTAINER_EMAIL = "martins7@tcd.ie"
 URL = "https://github.com/seankmartin/PythonUtils"
-DOWNLOAD_URL = "https://github.com/seankmartin/PythonUtils/archive/0.1.1.tar.gz"
-VERSION = "0.1.1"
+DOWNLOAD_URL = "https://github.com/seankmartin/PythonUtils/archive/0.1.2.tar.gz"
+VERSION = "0.1.2"
 
 INSTALL_REQUIRES = ["numpy", "matplotlib", "seaborn"]
 
