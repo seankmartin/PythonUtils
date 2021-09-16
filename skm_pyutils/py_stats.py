515425af8100d34fc7735247ec731f07fbda5ffe
Sean Martin <seankieran.m@hotmail.com>
2021-09-16 17:22:00 +0100

    Include wilcoxon test

--- skm_pyutils/py_stats.py
+++ skm_pyutils/py_stats.py
@@ -8,11 +8,85 @@ import seaborn as sns
 from skm_pyutils.py_plot import UnicodeGrabber
 from skm_pyutils.py_table import list_to_df
 
+def plot_dists(x, y, ax=None, **fmt_kwargs):
+    """Plot distrubtions of two arrays."""
+    palette = fmt_kwargs.get("palette", "dark")
+    context = fmt_kwargs.get("context", "paper")
+    group1_name = fmt_kwargs.get("group1", "1")
+    group2_name = fmt_kwargs.get("group2", "2")
+    vname = fmt_kwargs.get("value", "values")
+    sns.set_palette(palette)
+    if context == "paper":
+        sns.set_context(
+            "paper", font_scale=1.4, rc={"lines.linewidth": 3.2},
+        )
+    else:
+        sns.set_context(context)
+
+    if ax is None:
+        figure, ax = plt.subplots()
+    else:
+        figure = None
+    despine = fmt_kwargs.get("despine", True)
+    trim = fmt_kwargs.get("trim", True)
+    offset = fmt_kwargs.get("offset", None)
+
+    df_list = []
+    for val in x:
+        df_list.append([val, group1_name])
+    for val in y:
+        df_list.append([val, group2_name])
+
+    df = list_to_df(df_list, headers=[vname, "group"])
+    sns.kdeplot(data=df, x=vname, hue="group", multiple="stack", ax=ax)
+
+    ax.set_xlabel(vname)
+    if despine:
+        sns.despine(offset=offset, trim=trim)
+
+    return figure
+
+def plot_corr(x, y, ax=None, **fmt_kwargs):
+    """Plot correlation between two arrays."""
+    value1_name = fmt_kwargs.get("value1", "1")
+    value2_name = fmt_kwargs.get("value2", "2")
+    palette = fmt_kwargs.get("palette", "dark")
+    context = fmt_kwargs.get("context", "paper")
+    sns.set_palette(palette)
+    if context == "paper":
+        sns.set_context(
+            "paper", font_scale=1.4, rc={"lines.linewidth": 3.2},
+        )
+    else:
+        sns.set_context(context)
+
+    if ax is None:
+        figure, ax = plt.subplots()
+    else:
+        figure = None
+    despine = fmt_kwargs.get("despine", True)
+    trim = fmt_kwargs.get("trim", True)
+    offset = fmt_kwargs.get("offset", None)
+
+    sns.regplot(x=x, y=y, ax=ax, truncate=False, order=1)
+
+    extents_x = (0.97 * np.min(x), np.max(x) * 1.03)
+    extents_y = (0.97 * np.min(y), np.max(y) * 1.03)
+
+    ax.set_xlim(extents_x)
+    ax.set_ylim(extents_y)
+    ax.set_xlabel(value1_name)
+    ax.set_ylabel(value2_name)
+    if despine:
+        sns.despine(offset=offset, trim=trim)
+
+    return figure
 
 def corr(x, y, fmt_kwargs=None, do_plot=False, ax=None, **kwargs):
     """
     Compute correlation between x and y.
 
+    Based on the method passed as a keyword arg. By default Pearsons.
     Also returns a formatted string representation.
 
     Parameters
@@ -38,6 +112,7 @@ def corr(x, y, fmt_kwargs=None, do_plot=False, ax=None, **kwargs):
         An axes to plot into.
     **kwargs : keyword arguments
         These are passed to pingouin.corr
+        Of particular is method - the correlation to run.
 
     Returns
     -------
@@ -131,34 +206,7 @@ def corr(x, y, fmt_kwargs=None, do_plot=False, ax=None, **kwargs):
 
     figure = None
     if do_plot:
-
-        palette = fmt_kwargs.get("palette", "dark")
-        context = fmt_kwargs.get("context", "paper")
-        sns.set_palette(palette)
-        if context == "paper":
-            sns.set_context(
-                "paper", font_scale=1.4, rc={"lines.linewidth": 3.2},
-            )
-        else:
-            sns.set_context(context)
-
-        if ax is None:
-            figure, ax = plt.subplots()
-        despine = fmt_kwargs.get("despine", True)
-        trim = fmt_kwargs.get("trim", True)
-        offset = fmt_kwargs.get("offset", None)
-
-        sns.regplot(x=x, y=y, ax=ax, truncate=False, order=1)
-
-        extents_x = (0.97 * np.min(x), np.max(x) * 1.03)
-        extents_y = (0.97 * np.min(y), np.max(y) * 1.03)
-
-        ax.set_xlim(extents_x)
-        ax.set_ylim(extents_y)
-        ax.set_xlabel(value1_name)
-        ax.set_ylabel(value2_name)
-        if despine:
-            sns.despine(offset=offset, trim=trim)
+        figure = plot_corr(x, y, ax=ax, **fmt_kwargs)
 
     results = {
         "results": results_df,
@@ -173,6 +221,7 @@ def mwu(x, y, fmt_kwargs=None, do_plot=False, ax=None, **kwargs):
     """
     Compute the Mann-Whitney U Test.
 
+    This is the independent t-test non-parametric version.
     Also returns a formatted string for paper reporting.
 
     Parameters
@@ -289,35 +338,139 @@ def mwu(x, y, fmt_kwargs=None, do_plot=False, ax=None, **kwargs):
 
     figure = None
     if do_plot:
+        figure = plot_corr(x, y, ax=ax, **fmt_kwargs)
 
-        palette = fmt_kwargs.get("palette", "dark")
-        context = fmt_kwargs.get("context", "paper")
-        sns.set_palette(palette)
-        if context == "paper":
-            sns.set_context(
-                "paper", font_scale=1.4, rc={"lines.linewidth": 3.2},
-            )
-        else:
-            sns.set_context(context)
+    results = {
+        "results": results_df,
+        "output": results_str,
+        "figure": figure,
+    }
 
-        if ax is None:
-            figure, ax = plt.subplots()
-        despine = fmt_kwargs.get("despine", True)
-        trim = fmt_kwargs.get("trim", True)
-        offset = fmt_kwargs.get("offset", None)
+    return results
 
-        df_list = []
-        for val in x:
-            df_list.append([val, group1_name])
-        for val in y:
-            df_list.append([val, group2_name])
 
-        df = list_to_df(df_list, headers=[vname, "group"])
-        sns.kdeplot(data=df, x=vname, hue="group", multiple="stack", ax=ax)
+def wilcoxon(x, y, fmt_kwargs=None, do_plot=False, ax=None, **kwargs):
+    """
+    Compute the wilcoxon signed-rank test.
 
-        ax.set_xlabel(vname)
-        if despine:
-            sns.despine(offset=offset, trim=trim)
+    This is the non-parametric paired t-test.
+    Also returns a formatted string for paper reporting.
+
+    Parameters
+    ----------
+    x : array_like
+        First set of observations.
+    y: array_like
+        Second set of observations.
+    fmt_kwargs : dict, optional
+        A dictionary of kwargs to control the formatting.
+        value - the name of the values being tested
+        unit - the unit of the values being tested
+        group1 - the name of x
+        group2 - the name of y
+        signif - the significance level (float)
+        n_decimals - the number of decimal places to print (int)
+        n_pdecimals - the number of decimal places to print for p (int)
+        show_quartiles - include quartiles in report (bool)
+        do_print - print the report string (bool)
+    do_plot : bool, optional
+        Whether or not to plot the result.
+    ax : axes object, optional
+        An axes to plot into.
+    **kwargs : keyword arguments
+        These are passed to pingouin.corr
+
+    Returns
+    -------
+    dict with keys:
+        "results" : pd.DataFrame
+            The dataframe of results
+        "output" : str
+            A string to describe the test result for reporting
+        "figure" : matplotlib.pyplot.figure or None
+            The returned figure plotted into.
+
+    See also
+    --------
+    pinouin.wilcoxon
+    scipy.stats.wilcoxon
+
+    """
+    if fmt_kwargs is None:
+        fmt_kwargs = {}
+    vname = fmt_kwargs.get("value", "values")
+    unit_name = fmt_kwargs.get("unit", "")
+    if unit_name != "":
+        unit_name = " " + unit_name + ", "
+    else:
+        unit_name = ", "
+    group1_name = fmt_kwargs.get("group1", "1")
+    group2_name = fmt_kwargs.get("group2", "2")
+    signif_level = fmt_kwargs.get("signif", 0.05)
+    n_decimals = fmt_kwargs.get("n_decimals", 2)
+    n_pdecimals = fmt_kwargs.get("n_pdecimals", 3)
+    show_quartiles = fmt_kwargs.get("show_quartiles", True)
+    do_print = fmt_kwargs.get("do_print", True)
+
+    sided = kwargs.get("alternative", "two-sided")
+
+    results_df = pingouin.wilcoxon(x, y, **kwargs)
+    U = results_df["W-val"].values[0]
+    P = np.round(results_df["p-val"].values[0], n_pdecimals)
+    cl = np.round(results_df["CLES"].values[0], n_decimals)
+    median1 = np.round(np.median(x), n_decimals)
+    lowerq1, higherq1 = np.round(np.percentile(x, [25, 75]), n_decimals)
+    lowerq2, higherq2 = np.round(np.percentile(y, [25, 75]), n_decimals)
+    median2 = np.round(np.median(y), n_decimals)
+
+    sample_size1 = len(x)
+    sample_size2 = len(y)
+    n1 = "n" + UnicodeGrabber.to_sub(1)
+    n2 = "n" + UnicodeGrabber.to_sub(2)
+    if sample_size1 == sample_size2:
+        sample_str = f"{n1} = {n2} = {sample_size1}"
+    else:
+        sample_str = f"{n1} = {sample_size1}, {n2} = {sample_size2}"
+
+    stats_str = (
+        "(Wilcoxon signed-rank "
+        + "W"
+        + f" = {U}, CLES = {cl}, {sample_str}, "
+        + "\u0070"
+        + f" = {P}"
+    )
+
+    if sided == "two-sided":
+        stats_str += " two-tailed)."
+    else:
+        stats_str += " one-tailed)."
+
+    if P < signif_level:
+        differ_str = "differed significantly"
+    else:
+        differ_str = "did not differ significantly"
+
+    if show_quartiles:
+        results_str = (
+            f"Median [quartiles] {vname} in groups {group1_name} and {group2_name} were "
+            + f"{median1} [{lowerq1}, {higherq1}] and {median2} [{lowerq2}, {higherq2}]"
+            f"{unit_name}respectively; "
+            f"the distributions in the two groups {differ_str} {stats_str}"
+        )
+    else:
+        results_str = (
+            f"Median {vname} in groups {group1_name} and {group2_name} were "
+            + f"{median1} and {median2}"
+            f"{unit_name}respectively; "
+            f"the distributions in the two groups {differ_str} {stats_str}"
+        )
+
+    if do_print:
+        print(results_str)
+
+    figure = None
+    if do_plot:
+        figure = plot_corr(x, y, ax=ax, **fmt_kwargs)
 
     results = {
         "results": results_df,
